67cd9fd94e70727c0c1a14ba595922236b299dad
arvinsaurus <arvinaradhana165@gmail.com>
2021-12-26 01:23:42 +0700

    progress capek

--- resources/views/satwa.blade.php
+++ resources/views/satwa.blade.php
@@ -20,28 +20,52 @@
         <div class="row d-flex justify-content-center">
             <div class="col-12">
                 <div class="row d-flex justify-content-center">
-                    <div class="col-5">
-                        <a href="" class="satwa1">
-                            <img src="img/Satwa 04.png" alt="" class="w-100"> 
+                    <div class="col-5 position-relative">
+                        <a href="https://id.wikipedia.org/wiki/Badak_jawa">
+                            <img src="img/Satwa 04.png" alt="" class="w-100">
+                            <div class="overlay text-center position-absolute top-0 h-100 w-100 d-flex flex-column align-items-center">
+                                <div class="text my-auto">
+                                    <h6 class="m-0">Badak Jawa</h6>
+                                    <p>Rhinoceros sondaicus</p>
+                                </div>
+                            </div> 
                         </a>
                     </div>
                     <div class="col-7">
                         <div class="row d-flex flex-column justify-content-between h-100">
-                            <div class="col">
-                                <a href="" class="satwa2">
+                            <div class="col position-relative">
+                                <a href="https://id.wikipedia.org/wiki/Anjing_hutan" class="satwa2">
                                     <img src="img/Satwa 03.png" alt="" class="w-100">
+                                    <div class="overlay text-center position-absolute top-0 h-100 w-100 d-flex flex-column align-items-center">
+                                        <div class="text my-auto">
+                                            <h6 class="m-0">Anjing Hutan</h6>
+                                            <p>Cuon alpinus javanicus</p>
+                                        </div>
+                                    </div>
                                 </a>
                             </div>
                             <div class="col h-100">
                                 <div class="row d-flex justify-content-center h-100">
-                                    <div class="col h-100">
-                                        <a href="" class="satwa3">
+                                    <div class="col h-100 position-relative">
+                                        <a href="https://id.wikipedia.org/wiki/Owa_jawa" class="satwa3">
                                             <img src="img/Satwa 02.png" class="w-100 h-100" alt="">
+                                            <div class="overlay text-center position-absolute top-0 h-100 w-100 d-flex flex-column align-items-center">
+                                                <div class="text my-auto">
+                                                    <h6 class="m-0">Owa Jawa</h6>
+                                                    <p>Hylobates moloch</p>
+                                                </div>
+                                            </div>
                                         </a>
                                     </div>
-                                    <div class="col h-100">
-                                        <a href="" class="satwa4">
-                                            <img src="img/Satwa 02.png" class="w-100 h-100" alt="">
+                                    <div class="col h-100 position-relative">
+                                        <a href="https://id.wikipedia.org/wiki/Surili" class="satwa4">
+                                            <img src="img/Satwa 01.png" class="w-100 h-100" alt="">
+                                            <div class="overlay text-center position-absolute top-0 h-100 w-100 d-flex flex-column align-items-center">
+                                                <div class="text my-auto">
+                                                    <h6 class="m-0">Surili</h6>
+                                                    <p>Presbytis aigula</p>
+                                                </div>
+                                            </div>
                                         </a>
                                     </div>
                                 </div>
@@ -49,10 +73,11 @@
                         </div>
                     </div>
                 </div>
-                <h4 class="text-center w-100 my-5">Dan masih banyak lainnya...</h4>
             </div>
         </div>
     </div>
+
+    <h4 class="text-center w-100 my-5">Dan masih banyak lainnya...</h4>
 
     <!-- News -->
     <div class="container mb-5">
